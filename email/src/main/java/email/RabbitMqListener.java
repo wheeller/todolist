5d363f0d73f9f4758d6f6912e6d61f5b59bdcd9a
Andrey <thunderbass@mail.ru>
2022-04-15 17:32:31 +0300

    handle exceptions in email service

--- email/src/main/java/email/RabbitMqListener.java
+++ email/src/main/java/email/RabbitMqListener.java
@@ -6,16 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.ImmediateAcknowledgeAmqpException;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.mail.MailException;
 import org.springframework.stereotype.Component;
 
 
 @EnableRabbit
 @Component
-@ConditionalOnProperty(
-        value="email.message-service",
-        havingValue = "rabbit")
 public class RabbitMqListener {
     final EmailService emailService;
     Logger logger = LoggerFactory.getLogger(RabbitMqListener.class);
@@ -30,10 +26,9 @@ public class RabbitMqListener {
         try {
             logger.info("Received message from userQueue {}", message);
             emailService.sendSimpleMessage(message);
-        } catch (MailException ex) {
+        } catch (RuntimeException ex) {
             // against infinite listener restarts
             throw new ImmediateAcknowledgeAmqpException(ex.getMessage());
         }
     }
-
 }
